9b1f3c38fbfd5b616fdb5474bcdcae64d72a6c79
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-06-18 20:08:37 -0500

    added main feature

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import socket
 
 HOST = "localhost"
-# PORT = 8888
-PORT = 8889
+PORT = 8810
+# PORT = 8889
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -19,13 +19,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             method = data.decode().split()[0]
             url = data.decode().split()[1]
             protocol = url.split(":")[0]
-            host = url.split(":")[1].split("/")[2]  # 0, 1 -> are '/' '/'
+            # host = url.split(":")[1].split("/")[2]  # 0, 1 -> are '/' '/'
+            host = data.decode().split()[4]
+            path = url.split(
+                f"{host}"
+            )[1]  # WARNING: change this, what if user has the host name inside path
+            # print(path)
             target_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             target_socket.connect((host, 80))
-            proxy_http_request = f"{method} / HTTP/1.1 \r\nHost: {host}\r\n\r\n"  # \r\n (CRLF) is -> moving cursor to the beginning (\r) and starting new line (required by HTTP spec)
+
+            # NOTE:
+            """
+            \r\n (CRLF) is -> moving cursor to the beginning (\r) and starting new line (required by HTTP spec)
+                - We can actually see the raw response if we do curl --trace (look at the .. in the response header and the hex that corresponds to it 0d and 0a)
+                    these both translate to \r\n ~intresting
+            """
+            proxy_http_request = f"{method} {path} HTTP/1.1 \r\nHost: {host}\r\n\r\n"
             # print(proxy_http_request)
             target_socket.sendall(proxy_http_request.encode())
             target_socket_response = target_socket.recv(4096)
             print(target_socket_response.decode())
-            # print(host, method, url, protocol)
+            print(host, method, url, protocol)
         print("user disconnected")
